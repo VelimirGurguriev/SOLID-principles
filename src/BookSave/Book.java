da90309978142b35701dad25d6414ce258dbc5a1
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-10 16:50:01 +0300

    added constructors and getters/setters

--- src/BookSave/Book.java
+++ src/BookSave/Book.java
@@ -5,4 +5,47 @@ public class Book {
     private String author;
     private double price;
     private int pages;
+
+    public Book() {
+
+    }
+
+    public Book(String title, String author, double price, int pages) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.pages = pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
 }
