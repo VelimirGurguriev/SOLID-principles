7cb7186ed3a15484621c7a52e56b6f615a8047b5
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-10 17:06:11 +0300

    implemented method to save Book object to txt file

--- src/BookSave/SaveToDB.java
+++ src/BookSave/SaveToDB.java
@@ -1,13 +1,22 @@
 package BookSave;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class SaveToDB {
-    //Book book;
 
     public SaveToDB() {
 
     }
 
     public void saveToFile(Book book) {
-        
+        String fileName = "database.txt";
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            oos.writeObject(book);
+            System.out.println("Book saved successfully to database.");
+        } catch (IOException exc) {
+            System.out.println("Error during saving: " + exc);
+        }
     }
 }
