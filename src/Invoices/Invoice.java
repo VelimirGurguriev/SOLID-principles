7caa97c3996058534eefb40e8a6c290083f3a060
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-11 16:07:13 +0300

    added Invoice class with constructor and parameters

---
+++ src/Invoices/Invoice.java
@@ -0,0 +1,15 @@
+package Invoices;
+
+public class Invoice {
+    private double amount;
+    private String customerName;
+    private String companyName;
+    private String date;
+
+    public Invoice(double amount, String customerName, String companyName, String date) {
+        this.amount = amount;
+        this.customerName = customerName;
+        this.companyName = companyName;
+        this.date = date;
+    }
+}
