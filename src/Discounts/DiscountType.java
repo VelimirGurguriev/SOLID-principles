fe20f3afc239a90e0491148f43dd191cc26468f4
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-12 16:36:51 +0300

    added DiscountType enum class

---
+++ src/Discounts/DiscountType.java
@@ -0,0 +1,18 @@
+package Discounts;
+
+public enum DiscountType {
+    CHILD(0.3),
+    STUDENT(0.1),
+    SENIOR(0.2),
+    DISABLED(0.5);
+
+    private final double percentage;
+
+    DiscountType(double percentage) {
+        this.percentage = percentage;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+}
