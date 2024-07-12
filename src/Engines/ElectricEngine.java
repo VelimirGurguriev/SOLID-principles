5fc495ee31f03d362e352dc776af9aa4401fe9c7
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-12 19:05:19 +0300

    implemented dependancy of Electric engine methods

--- src/Engines/ElectricEngine.java
+++ src/Engines/ElectricEngine.java
@@ -1,4 +1,28 @@
 package Engines;
 
-public class ElectricEngine {
+public class ElectricEngine extends Engine implements Electricable {
+
+    public ElectricEngine(int engineWeight, int engineHorsepower, String engineBrand) {
+        super(engineWeight, engineHorsepower, engineBrand);
+    }
+
+    @Override
+    public boolean hasFuelEmissions() {
+        return false;
+    }
+
+    @Override
+    public void turnOn() {
+        System.out.println("Engine turned on with electrical power");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("Engine turned off by stopping electrical power supply");
+    }
+
+    @Override
+    public void recharge() {
+        System.out.println("Engine battery needs to recharge with electricity");
+    }
 }
