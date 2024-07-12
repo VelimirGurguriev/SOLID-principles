04d8c6840b742984856a0a3277c32fa9fdf5fe2b
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-12 20:41:57 +0300

    added swimable and flyable interfaces for different types of birds

--- src/Penguins/Bird.java
+++ src/Penguins/Bird.java
@@ -1,4 +1,13 @@
 package Penguins;
 
-public class Bird {
+public abstract class Bird {
+
+    public void eat() {
+        System.out.println("started eating");
+    }
+
+    public void sleep() {
+        System.out.println("started sleeping");
+    }
+
 }
