666b39692db3f33c7be4c57cbeb90a35eed8ab57
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-13 20:25:06 +0300

    added TV test class

---
+++ src/SwitchLight/TV.java
@@ -0,0 +1,13 @@
+package SwitchLight;
+
+public class TV implements Switchable {
+    @Override
+    public void turnOn() {
+        System.out.println("TV is on");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("TV is off");
+    }
+}
