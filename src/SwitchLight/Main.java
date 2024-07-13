af012228eb8d26022db5333d53f41410b3d1ffcf
velimir.gurguriev <velimir.gurguriev@gmail.com>
2024-07-13 20:26:04 +0300

    tested tv class

--- src/SwitchLight/Main.java
+++ src/SwitchLight/Main.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Switchable bulb = new Bulb();
         SwitchDevice bulbSwitch = new SwitchDevice(bulb);
+        bulbSwitch.operate();
+        bulbSwitch.operate();
 
-        bulbSwitch.operate();
-        bulbSwitch.operate();
+        Switchable tv = new TV();
+        SwitchDevice tvSwitch = new SwitchDevice(tv);
+        tvSwitch.operate();
+        tvSwitch.operate();
     }
 }
